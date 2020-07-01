1f22678cedbd17f669cf30c3e3b696fc1dfcb732
patrick9958 <patrick.neggie@gmail.com>
2020-07-01 18:36:05 -0400

    added num_keys class, finished num_click_handler

--- calc_files/calc.js
+++ calc_files/calc.js
@@ -1,5 +1,19 @@
-var num_keys = [$('#button0'), $('#button1'), $('#button2'), $('#button3'), $('#button4'),
-    $('#button5'), $('#button6'), $('#button7'), $('#button8'), $('#button9')];
+var zero = $('#button0');
+var one = $('#button1');
+var two = $('#button2');
+var three = $('#button3');
+var four = $('#button4');
+var five = $('#button5');
+var six = $('#button6');
+var seven = $('#button7');
+var eight = $('#button8');
+var nine = $('#button9');
+
+var num_keys = [zero, one, two, three, four, five, six, seven, eight, nine];
+for (var i = 0; i < num_keys.length; i++) {
+    num_keys[i].addClass('num_keys');
+}
+
 var add_key = $('#addButton');
 var sub_key = $('#subtractButton');
 var multiply_key = $('#multiplyButton');
@@ -10,7 +24,13 @@ var display_elem = $('#display');
 
 function num_click_handler() {
     console.log('num_click_handler called');
-    display_elem.val(3)
+    if (display_elem.val()) {
+        var new_val = display_elem.val() + $(this).val();
+        display_elem.val(new_val);
+    }
+    else {
+        display_elem.val($(this).val());
+    }
 }
 
 function add_click_handler() {
@@ -29,14 +49,4 @@ function divide_click_handler() {
 
 }
 
-function global_click_handler() {
-    if (num_keys.includes($(this))) {
-        console.log('num_key pressed');
-    }
-}
-
-if ($(':button').click(global_click_handler)) {
-    console.log('button pressed');
-}
-
-$('#button3').click(num_click_handler);
+$('.num_keys').click(num_click_handler);
